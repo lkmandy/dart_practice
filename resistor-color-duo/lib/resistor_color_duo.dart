69a365d82dc2e746f9d3581dae5ab7ffef8e8a6b
Amanda Shafack <shafack.likhene@gmail.com>
2021-08-10 00:01:30 +0100

    Make all test to pass and optimize code

--- resistor-color-duo/lib/resistor_color_duo.dart
+++ resistor-color-duo/lib/resistor_color_duo.dart
@@ -1,19 +1,10 @@
-import 'dart:io';
-import 'dart:core';
-
 
 class ResistorColorDuo {
-
-  ResistorColorDuo();
-  // Put your code here
-  List<String> colors = ['Black', 'Brown', 'Red',' Orange', 'Yellow', 'Green', 'Blue', 'Violet', 'Grey', 'White'];
   
- int value(List<String> colorInput){
-   colorInput = stdin.readLineSync();
+  List<String> colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
   
-    int colorOne = colors.indexOf(colorInput[0]);
-    int colorTwo = colors.indexOf(colorInput[1]);
-
-    return int.parse('{$colorOne$colorTwo}');
+  int value(List<String> colorInput){
+    int result = (colors.indexOf(colorInput[0])*10) + colors.indexOf(colorInput[1]);
+    return result;
   }
 }
