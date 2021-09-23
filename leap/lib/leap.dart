2db8ffa16d27a5519868391ce86d4fe7eaae931e
Amanda Shafack <shafack.likhene@gmail.com>
2021-09-23 23:20:15 +0100

    Optimize program and add user friendly outputs

--- leap/lib/leap.dart
+++ leap/lib/leap.dart
@@ -1,13 +1,21 @@
 class Leap {
-  // Put your code here
-  int year;
-
-  bool leapYear(int year){
-    if ((year % 4 == 0) && (year % 400 == 0) ){
-      if ((year % 10 != 0) ){
+  bool leapYear(int year) {
+    if ((year % 4 == 0) && (year % 400 == 0)) {
+      if ((year % 10 != 0)) {
         return true;
       }
     }
     return false;
+  }
+}
+
+void main() {
+  Leap testYear = Leap();
+  const int year = 2021;
+
+  if (testYear.leapYear(year)) {
+    print('$year is a leap year');
+  } else {
+    print('$year is not a leap year');
   }
 }
