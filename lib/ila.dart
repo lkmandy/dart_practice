71944a23c595ca93ff8ac7848493f96f73d2e173
Amanda Shafack <shafack.likhene@gmail.com>
2022-05-07 21:38:01 +0100

    Setup a command-line project from a template

---
+++ lib/ila.dart
@@ -0,0 +1,21 @@
+import 'package:calculator/calculator.dart';
+
+num calculate() {
+  return add(25, 4);
+}
+
+num calculateAdd() {
+  return add(25, 4);
+}
+
+num calculateSubtract() {
+  return subtract(25, 4);
+}
+
+num calculateDivide() {
+  return divide(25, 4);
+}
+
+num calculateMultiply() {
+  return multiply(25, 4);
+}
